3317021c526eb3d5c460c4c60c3acb44342b5663
钱 浩 <qianhao100083@foxmail.com>
2024-11-06 05:03:14 +0000

    chore: main.ts comment

--- apps/web-antd/src/main.ts
+++ apps/web-antd/src/main.ts
@@ -7,7 +7,7 @@ import { overridesPreferences } from './preferences';
  * 应用初始化完成之后再进行页面加载渲染
  */
 async function initApplication() {
-  // name用于指定项目唯一标识
+  // namespace用于指定项目唯一标识
   // 用于区分不同项目的偏好设置以及存储数据的key前缀以及其他一些需要隔离的数据
   const env = import.meta.env.PROD ? 'prod' : 'dev';
   const appVersion = import.meta.env.VITE_APP_VERSION;
